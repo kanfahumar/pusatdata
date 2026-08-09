76e48176854ebb5881afd4c86c2e286b8dd340e5
kanfahumar <kanfahumar@gmail.com>
2026-08-09 22:02:40 +0700

    3 level

--- data.js
+++ data.js
@@ -3,23 +3,109 @@ const direktoriDinas = [
         id: "pm",
         namaBidang: "Penanaman Modal (PM)",
         deskripsi: "Arsip kegiatan, SPJ, data, dan keuangan bidang penanaman modal.",
-        tahunan: [
+        timKerja: [
             {
-                tahun: "2026",
-                subFolder: [
-                    { nama: "Dokumentasi", link: "https://drive.google.com/drive/folders/LINK_DOKUMENTASI_2026" },
-                    { nama: "SPJ", link: "https://drive.google.com/drive/folders/LINK_SPJ_2026" },
-                    { nama: "Data", link: "https://drive.google.com/drive/folders/LINK_DATA_2026" },
-                    { nama: "Keuangan", link: "https://drive.google.com/drive/folders/LINK_KEUANGAN_2026" }
+                id: "pm-tim1",
+                namaTim: "Tim Kerja 1",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
                 ]
             },
             {
-                tahun: "2025",
-                subFolder: [
-                    { nama: "Dokumentasi", link: "https://drive.google.com/drive/folders/LINK_DOKUMENTASI_2025" },
-                    { nama: "SPJ", link: "https://drive.google.com/drive/folders/LINK_SPJ_2025" },
-                    { nama: "Data", link: "https://drive.google.com/drive/folders/LINK_DATA_2025" },
-                    { nama: "Keuangan", link: "https://drive.google.com/drive/folders/LINK_KEUANGAN_2025" }
+                id: "pm-tim2",
+                namaTim: "Tim Kerja 2",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: "ptsp1",
+        namaBidang: "PTSP 1",
+        deskripsi: "Arsip pelayanan perizinan dan non-perizinan jalur pertama.",
+        timKerja: [
+            {
+                id: "ptsp1-tim1",
+                namaTim: "Tim Kerja 1",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "ptsp1-tim2",
+                namaTim: "Tim Kerja 2",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        id: "ptsp2",
+        namaBidang: "PTSP 2",
+        deskripsi: "Arsip pelayanan perizinan dan pengaduan jalur kedua.",
+        timKerja: [
+            {
+                id: "ptsp2-tim1",
+                namaTim: "Tim Kerja 1",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
+                ]
+            },
+            {
+                id: "ptsp2-tim2",
+                namaTim: "Tim Kerja 2",
+                tahunan: [
+                    {
+                        tahun: "2026",
+                        subFolder: [
+                            { nama: "Dokumentasi", link: "#" },
+                            { nama: "SPJ", link: "#" },
+                            { nama: "Data", link: "#" },
+                            { nama: "Keuangan", link: "#" }
+                        ]
+                    }
                 ]
             }
         ]
@@ -44,38 +130,6 @@ const direktoriDinas = [
         id: "umum",
         namaBidang: "Umum",
         deskripsi: "Dokumentasi kepegawaian, persuratan, dan urusan rumah tangga kantor.",
-        tahunan: [
-            {
-                tahun: "2026",
-                subFolder: [
-                    { nama: "Dokumentasi", link: "#" },
-                    { nama: "SPJ", link: "#" },
-                    { nama: "Data", link: "#" },
-                    { nama: "Keuangan", link: "#" }
-                ]
-            }
-        ]
-    },
-    {
-        id: "ptsp1",
-        namaBidang: "PTSP 1",
-        deskripsi: "Arsip pelayanan perizinan dan non-perizinan jalur pertama.",
-        tahunan: [
-            {
-                tahun: "2026",
-                subFolder: [
-                    { nama: "Dokumentasi", link: "#" },
-                    { nama: "SPJ", link: "#" },
-                    { nama: "Data", link: "#" },
-                    { nama: "Keuangan", link: "#" }
-                ]
-            }
-        ]
-    },
-    {
-        id: "ptsp2",
-        namaBidang: "PTSP 2",
-        deskripsi: "Arsip pelayanan perizinan dan pengaduan jalur kedua.",
         tahunan: [
             {
                 tahun: "2026",
